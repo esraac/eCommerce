00ea4805ff9cf5be3c423fc86633ff0985fefa65
chari00001 <cagrilacin303@gmail.com>
2024-12-28 19:30:52 +0300

    token fix test

--- frontend/src/pages/Verify.jsx
+++ frontend/src/pages/Verify.jsx
@@ -1,42 +1,45 @@
-import React, { useContext, useEffect } from 'react'
-import { ShopContext } from '../context/ShopContext'
-import { useSearchParams } from 'react-router-dom'
-import axios from 'axios'
+import React, { useContext, useEffect } from "react";
+import { ShopContext } from "../context/ShopContext";
+import { useSearchParams } from "react-router-dom";
+import axios from "axios";
 
 export default function Verify() {
+  const { navigate, token, setCartItems, backendUrl } = useContext(ShopContext);
+  const [searchParams, setSearchParams] = useSearchParams();
 
-  const {navigate, token, setCartItems, backendUrl} = useContext(ShopContext)
-  const [searchParams, setSearchParams] = useSearchParams()
+  const success = searchParams.get("success");
+  const orderId = searchParams.get("orderId");
 
-  const success = searchParams.get('success')
-  const orderId = searchParams.get('orderId')
-
-  const verifyPayment = async ()=>{
+  const verifyPayment = async () => {
     try {
-      if(!token){
-        return null
+      if (!token) {
+        return null;
       }
-      const response = await axios.post(backendUrl+ '/api/order/verifyStripe', {success, orderId}, {headers:{token}})
-      if(response.data.success){
-        setCartItems({})
-        navigate('/orders')
-      }else{
-        navigate('/')
+      const response = await axios.post(
+        backendUrl + "/api/order/verifyStripe",
+        { success, orderId },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      if (response.data.success) {
+        setCartItems({});
+        navigate("/orders");
+      } else {
+        navigate("/");
       }
     } catch (error) {
-      console.log(error)
-      toast.error(error.message)
+      console.log(error);
+      toast.error(error.message);
     }
-  }
+  };
 
-  useEffect(()=>{
-    verifyPayment()
-  },[token])
+  useEffect(() => {
+    verifyPayment();
+  }, [token]);
 
-
-  return (
-    <div>
-      
-    </div>
-  )
+  return <div></div>;
 }
